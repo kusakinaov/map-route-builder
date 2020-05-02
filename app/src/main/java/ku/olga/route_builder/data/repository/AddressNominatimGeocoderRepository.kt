9413bf7debde328594b9fa72c39b215410a5daee
kusakinaov <kusakinaov@gmail.com>
2020-05-02 22:22:32 +0300

    changed method for converting address to search address for nominatin geocoder

--- app/src/main/java/ku/olga/route_builder/data/repository/AddressNominatimGeocoderRepository.kt
+++ app/src/main/java/ku/olga/route_builder/data/repository/AddressNominatimGeocoderRepository.kt
@@ -1,7 +1,10 @@
 package ku.olga.route_builder.data.repository
 
+import android.location.Address
+import ku.olga.route_builder.domain.model.SearchAddress
 import ku.olga.route_builder.domain.repository.AddressRepository
 import org.osmdroid.bonuspack.location.GeocoderNominatim
+import java.lang.StringBuilder
 
 class AddressNominatimGeocoderRepository(val geocoderNominatim: GeocoderNominatim) :
     AddressRepository {
@@ -12,4 +15,23 @@ class AddressNominatimGeocoderRepository(val geocoderNominatim: GeocoderNominati
     override suspend fun searchAddress(lat: Double, lon: Double) =
         geocoderNominatim.getFromLocation(lat, lon, MAX_ADDRESS_SEARCH_RESULTS)
             .map { it.toSearchAddress() }
+
+    private fun Address.toSearchAddress() = SearchAddress(buildPostalAddress(), latitude, longitude)
+
+    private fun Address.buildPostalAddress(): String {
+        val builder = StringBuilder()
+        if (extras?.containsKey(NOMINATIM_EXTRAS_DISPLAY_NAME) == true) {
+            builder.append(extras.getString(NOMINATIM_EXTRAS_DISPLAY_NAME))
+        } else {
+            for (i in 0..maxAddressLineIndex) {
+                if (builder.isNotEmpty()) builder.append(", ")
+                builder.append(getAddressLine(i))
+            }
+        }
+        return builder.toString()
+    }
+
+    companion object {
+        const val NOMINATIM_EXTRAS_DISPLAY_NAME = "display_name"
+    }
 }
