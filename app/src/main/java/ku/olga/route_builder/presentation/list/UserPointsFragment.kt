ba19b34eaa522abb5c65a64e0c898b30ba05bf44
kusakinaov <kusakinaov@gmail.com>
2020-05-10 20:00:37 +0300

    inject user points presenter

--- app/src/main/java/ku/olga/route_builder/presentation/list/UserPointsFragment.kt
+++ app/src/main/java/ku/olga/route_builder/presentation/list/UserPointsFragment.kt
@@ -6,12 +6,27 @@ import android.view.*
 import ku.olga.route_builder.R
 import ku.olga.route_builder.presentation.App
 import ku.olga.route_builder.presentation.base.BaseFragment
+import ku.olga.route_builder.presentation.dagger.component.DaggerPointComponent
+import ku.olga.route_builder.presentation.dagger.component.PointComponent
+import ku.olga.route_builder.presentation.dagger.module.ApplicationModule
+import javax.inject.Inject
 
 class UserPointsFragment : BaseFragment() {
-    private val userPointsPresenter = UserPointsPresenter(App.pointsRepository)
+    private lateinit var pointsComponents: PointComponent
+    @Inject
+    lateinit var userPointsPresenter: UserPointsPresenter
+
     private var userPointsView: UserPointsView? = null
 
     override fun getTitle(resources: Resources) = resources.getString(R.string.ttl_point_list)
+
+    override fun inject() {
+        pointsComponents =
+            DaggerPointComponent.builder()
+                .applicationModule(ApplicationModule(App.application))
+                .build()
+        pointsComponents.inject(this)
+    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
