cac8f645e424aa19c3fb7db24c623bae854234ec
kusakinaov <kusakinaov@gmail.com>
2020-05-10 18:58:55 +0300

    added address module

---
+++ app/src/main/java/ku/olga/route_builder/presentation/dagger/module/AddressModule.kt
@@ -0,0 +1,21 @@
+package ku.olga.route_builder.presentation.dagger.module
+
+import dagger.Module
+import dagger.Provides
+import ku.olga.route_builder.data.repository.AddressNominatimGeocoderRepository
+import ku.olga.route_builder.domain.repository.AddressRepository
+import org.osmdroid.bonuspack.location.GeocoderNominatim
+import java.util.*
+import javax.inject.Singleton
+
+@Module
+class AddressModule {
+    @Singleton
+    @Provides
+    fun providesAddressRepository(geocoderNominatim: GeocoderNominatim): AddressRepository =
+        AddressNominatimGeocoderRepository(geocoderNominatim)
+
+    @Provides
+    fun providesGeocoderNominatim() =
+        GeocoderNominatim(Locale.getDefault(), "MapRouteBuilderUserAgent")
+}
