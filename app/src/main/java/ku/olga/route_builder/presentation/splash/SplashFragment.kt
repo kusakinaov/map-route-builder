ae2287038713a49abc677a322af401ab32b5baff
kusakinaov <kusakinaov@gmail.com>
2020-06-12 12:17:20 +0300

    created progress view

--- app/src/main/java/ku/olga/route_builder/presentation/splash/SplashFragment.kt
+++ app/src/main/java/ku/olga/route_builder/presentation/splash/SplashFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
+import kotlinx.android.synthetic.main.fragment_splash.*
 import ku.olga.route_builder.R
 import ku.olga.route_builder.presentation.MainActivity
 import ku.olga.route_builder.presentation.base.BaseFragment
@@ -22,9 +23,12 @@ class SplashFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        view.postDelayed({
+        view.postDelayed({
 //            replaceFragment(UserPointsFragment.newInstance(), false)
-//        }, 1000)
+
+            viewProgress.setProgress(100)
+        }, 1000)
+
     }
 
     companion object {
